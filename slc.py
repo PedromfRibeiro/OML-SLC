adb58771f9244bd2c41867fb9b0e680ce9eac091
Gonçalo Almeida <a84610@alunos.uminho.pt>
2021-06-02 11:25:00 +0100

    Errors solved

--- slc.py
+++ slc.py
@@ -32,15 +32,19 @@ def sigmoid(s):
     if s>large: s=large
     return (1 / (1 + np.exp(-s)))
 
-def normalization(w):
-    return(w/np.linalg.norm(w,2))
 '''
 Cost/Loss funtion
 '''
 def cost(X, Y, N, w, b, v, c):
+    epsi = 1.e-12
     sum = 0
     for n in range(N):
         prediction = predict(X[n], J, w, b, v, c)
+        # verify if the value of the prediction is in [1e-12, 1-1e-12]
+        if prediction < epsi: 
+            prediction = epsi
+        if prediction > 1 - epsi:
+            prediction = 1 - epsi
         sum += Y[n] * np.log(prediction) + (1 - Y[n]) * np.log(1 - prediction)
     E = - sum / N
     return E
@@ -74,31 +78,42 @@ def predict(x, J, w, b, v, c):
 '''
 Calculate the gradients for w, b, v, c
 '''
-def gradient(x, y, v, h, prediction, eta):
+def gradient(x, y, v, h, prediction):
+    
     func = lambda a: a * (1 - a)
     diagonal = np.array([func(h_m) for h_m in h])
     matrix1 = np.diag(diagonal) # J x J
+    # np.tensordot
     matrix2 = np.tile(x, (J, 1))    
     matrix2 = np.transpose(matrix2) * v
     matrix2 = np.transpose(matrix2) # J x I
     delta = prediction - y
-    #delta=np.sign(delta)*min(0.1,np.abs(delta));
-    #delta = delta* eta
-    Gw = (delta) * np.matmul(matrix1, matrix2) # w's gradient
-    Gb = (delta) * np.dot(matrix1, v)          # b's gradient
-    Gv = (delta) * h                           # v's gradient
-    Gc = delta                             # c's gradient
+    Gw = delta * np.matmul(matrix1, matrix2) # w's gradient
+    Gb = delta * np.dot(matrix1, v)          # b's gradient
+    Gv = delta * h                           # v's gradient
+    Gc = delta                               # c's gradient
+    '''
+    delta = prediction - y
+    diagonal = np.multiply(h, (1 - h))
+    Gb = delta * np.multiply(v, diagonal)
+    Gw = np.tensordot(Gb, x, axes=0)
+    Gv = delta * h
+    Gc = delta    
+    '''
     return Gw, Gb, Gv, Gc
 
 '''
 Update w, b, v, c
 '''
 def update(x, y, J, eta, w, b, v, c): 
+    # calculate the gradients
     h = get_h(x, w, b) 
     prediction = predict(x, J, w, b, v, c)
-    Gw, Gb, Gv, Gc = gradient(x, y, v, h, prediction, eta)
+    Gw, Gb, Gv, Gc = gradient(x, y, v, h, prediction)
+    # update the first layer
     w = w - (eta * Gw) # w (t + 1)
     b = b - (eta * Gb) # b (t + 1)
+    # update the second layer
     v = v - (eta * Gv) # v (t + 1)
     c = c - (eta * Gc) # c (t + 1)
     return w, b, v, c
@@ -107,11 +122,12 @@ def update(x, y, J, eta, w, b, v, c):
 Run shallow logistic classifier
 '''
 def run_slc(X, Y, N, J, subloop, eta, max_iteration, w, b, v, c, errors):
-    epsi = 10e-3
+    #epsi = 10e-3
+    epsi = 0
     iteration = 0
     while (errors[-1] > epsi):
         for j in range(subloop):
-        # choose random data from dataset
+            # choose random data from dataset
             m = randint(0, N - 1)
             x = X[m]
             y = Y[m]
@@ -120,31 +136,12 @@ def run_slc(X, Y, N, J, subloop, eta, max_iteration, w, b, v, c, errors):
             # calculate error
         error = cost(X, Y, N, w, b, v, c)
         errors.append(error)
-        print('iter %d, cost=%f, eta=%e     \r' %(iteration,errors[-1],eta),end='')
-        iteration += 1
-        if (iteration > max_iteration):break
+        print('number of iterations = %d, cost = %f, eta = %e\r' %(iteration, errors[-1], eta), end='')
+        iteration += subloop
+        if (iteration > max_iteration):
+            break
     return w, b, v, c, errors
-
-def plot_data(row,col,n_row,n_col,data):
-    fig=plt.figure(figsize=(row,col))
-    for n in range(1, row*col +1):
-        img=np.reshape(data[n-1][:-1],(n_row,n_col))
-        fig.add_subplot(row, col, n)
-        plt.imshow(img,interpolation='none',cmap='binary')
-    plt.show()
-'''    
-def plot_tagged_data(row,col,n_row,n_col,X,Y,ew): 
-    fig=plt.figure(figsize=(row,col))
-    for n in range(row*col):
-        img=np.reshape(X[n],(n_row,n_col))
-        fig.add_subplot(row, col, n+1)
-        #if(Y[n]>0):#exact case
-        if(predict(X[n],ew)>0.5):
-            plt.imshow(img,interpolation='none',cmap='RdPu')
-        else:
-            plt.imshow(img,interpolation='none',cmap='cool')               
-    plt.show()
-'''    
+ 
 def plot_error(err):
     plt.plot(range(len(err)), err, marker='o')
     plt.xlabel('Iterations')
@@ -168,42 +165,43 @@ Main execution
 '''
 
 # load data from dataset
-N, n_row, n_col,data = load_data('./Data/XOR.txt')
-#N, n_row, n_col,data = load_data('./Data/line1500.txt')
+#dataset = './Data/XOR.txt'
+dataset = './Data/line600.txt'
+#dataset = './Data/square_circle.txt'
+N, n_row, n_col,data = load_data(dataset)
 N = int(N * 1.0)
 I = n_row * n_col
 X = data[:N, :-1] 
 Y = data[:N, -1]
-#eta=1.0;
-Nbiter=1200;
-subloop=200;
-# J = 1 for AND, J = 2 for XOR, J = 3 for line600
-J = 2 # number of neurons in the hidden layer
+np.place(Y, Y != 1, [0]) # replace labels -1 for 0
 
-# initialize w, b, v, c
-w = np.ones((J, I))
-#w=normalization(w)*0;
-b = np.ones(J)
-#b=normalization(b)*0;
-v = np.ones(J)
-#v=normalization(v)*0;
+J = 3 # number of neurons in the hidden layer 
+      # 1 for AND, 2 for XOR, 3 for line600
 
-c = 1
-#c=normalization(c)*0;
-eta = 1.0  # learning rate
+# initialize w, b, v and c with random values
+w = np.random.randn(J, I)
+b = np.random.randn(J)
+v = np.random.rand(J)
+c = np.random.rand(1)
+
+eta = 0.5 # learning rate
+
+Nbiter = 8000 # number of iterations
+subloop = 100 # only calculate the cost in each 100 iterations
 
 # initialize errors' list
 errors = []
 errors.append(cost(X, Y, N, w, b, v, c))
 
-w, b, v, c, errors = run_slc(X, Y, N, J, subloop, eta, Nbiter, w, b, v, c, errors);print("\n");eta=0.7*eta;
-w, b, v, c, errors = run_slc(X, Y, N, J, subloop, eta, Nbiter, w, b, v, c, errors);print("\n");eta=0.5*eta;
-w, b, v, c, errors = run_slc(X, Y, N, J, subloop, eta, Nbiter, w, b, v, c, errors);print("\n");eta=0.3*eta;
-w, b, v, c, errors = run_slc(X, Y, N, J, subloop, eta, Nbiter, w, b, v, c, errors);print("\n");eta=0.4*eta;
-#w, b, v, c, errors = run_slc(X, Y, N, J, subloop, eta, Nbiter, w, b, v, c, errors)
+w, b, v, c, errors = run_slc(X, Y, N, J, subloop, eta, Nbiter, w, b, v, c, errors); print("\n"); eta = 0.8 * eta
+w, b, v, c, errors = run_slc(X, Y, N, J, subloop, eta, Nbiter, w, b, v, c, errors); print("\n"); eta = 0.7 * eta
+#w, b, v, c, errors = run_slc(X, Y, N, J, subloop, eta, Nbiter, w, b, v, c, errors); print("\n"); eta = 0.6 * eta
+#w, b, v, c, errors = run_slc(X, Y, N, J, subloop, eta, Nbiter, w, b, v, c, errors); print("\n")
+
 plot_error(errors)
 
 print('in-samples error = %f ' % (cost(X, Y, N, w, b, v, c)))
-#print(ew)
+
+# calculate and show confusion matrix
 C = confusion(X, Y, N, J, w, b, v, c)
 print(C)
